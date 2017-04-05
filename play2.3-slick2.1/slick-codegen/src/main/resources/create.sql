2edf5a736c101da663e55b4cd2629704795f9a51
Naoki Takezoe <takezoe@gmail.com>
2017-04-05 10:33:54 +0900

    Update sample data

--- play2.3-slick2.1/slick-codegen/src/main/resources/create.sql
+++ play2.3-slick2.1/slick-codegen/src/main/resources/create.sql
@@ -4,9 +4,8 @@ create table COMPANIES (ID INTEGER NOT NULL, NAME VARCHAR NOT NULL);
 alter table USERS ADD CONSTRAINT IDX_USERS_FK0 FOREIGN KEY (COMPANY_ID) REFERENCES COMPANIES (ID);
 alter table COMPANIES ADD CONSTRAINT IDX_COMPANIES_PK PRIMARY KEY (ID);
 
-insert into COMPANIES values (1, 'Biz Reach');
+insert into COMPANIES values (1, 'BizReach');
 insert into COMPANIES values (2, 'Recruit');
-insert into COMPANIES values (3, 'DODA');
 
-insert into USERS(NAME, COMPANY_ID) values ('Naoki Takezoe', 1);
-insert into USERS(NAME) values ('Takako Shimamoto');
+insert into USERS(NAME, COMPANY_ID) values ('Taro Yamada', 1);
+insert into USERS(NAME) values ('Jiro Sato');
